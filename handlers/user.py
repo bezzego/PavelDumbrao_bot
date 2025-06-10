c5adb4f7c614418b406522221637c86d788f5514
Egor Bezborodov <egor.bezborodoff@gmail.com>
2025-06-10 19:48:57 +0300

    Изменение текста сообщения о закрытом чате: добавлено указание на отсутствие ссылки и контакт для связи.

--- handlers/user.py
+++ handlers/user.py
@@ -697,7 +697,9 @@ async def cmd_access_closed(message: types.Message):
             )
             invite_url = new_invite.invite_link
             db.set_invite_link(user_id, invite_url)
-        await message.answer(f"Вот ссылка на закрытый чат: {invite_url}")
+        await message.answer(
+            f"Вот ссылка на закрытый чат: пока пусто. Пишите сюда: @PavelDumbrao"
+        )
     else:
         from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
